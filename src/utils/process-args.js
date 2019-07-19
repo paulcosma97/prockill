cd73d6f9573f41eaa9a2dfb291e8c1a93416e863
Paul Cosma <paul.cosma97@gmail.com>
2019-07-19 14:30:53 +0300

    Add more options

--- src/utils/process-args.js
+++ src/utils/process-args.js
@@ -3,32 +3,81 @@ const nopt = require('nopt');
 const throwError = require('./throw-err');
 
 const knownOptions = {
-    p: [Number, Array]
+    'p': [Number, Array],
+    'pid': [Number, Array],
+    'n': [String, Array],
+    's': [null],
+    'h': [null]
 };
 
 const shortHands = {
-    port: ['-p']
+    'port': ['-p'],
+    'process-id': ['-pid'],
+    'name': ['-n'],
+    'silent': ['-s'],
+    'help': ['-h'],
 };
 
 const args = nopt(knownOptions, shortHands, process.argv).argv.cooked;
 
-if (!args.find(opt => opt === '-p')) {
-    throwError('You must specify at least one port');
+const initialOptions = {
+    ports: [],
+    processIds: [],
+    names: [],
+    s: false,
+    h: false
 }
 
+const mapOption = opt => {
+    const option = opt.split('-')[1];
+    let explicitOption = null;
+
+    switch (option) {
+        case 'p':
+            explicitOption = 'ports'
+            break;
+        case 'pid':
+            explicitOption = 'processIds'
+            break;
+        case 'n':
+            explicitOption = 'names'
+            break;
+        case 's':
+            explicitOption = 'silent'
+            break;
+        case 'h':
+            explicitOption = 'help'
+            break;
+    }
+
+    return {
+        option,
+        explicitOption,
+        types: knownOptions[option]
+    }
+};
+
 module.exports = (() => {
-    const out = {
-        ports: []
-    };
+    const out = { ...initialOptions };
 
-    for (let i = 1; i < args.length; i++) {
-        const value = args[i];
+    let lastArg = null;
+    for (const value of args) {
+        if (value.startsWith('-')) {
+            lastArg = mapOption(value);
+            
+            if (lastArg.types.includes(null)) {
+                out[lastArg.explicitOption] = true;
+            }
 
-        if (isNaN(+value)) {
-            throwError('Port options must have number values');
+            continue;
         }
 
-        out.ports.push(+value);
+        if (lastArg.types.includes(Array)) {
+            out[lastArg.explicitOption].push(value);
+            continue;
+        }
+
+        out[lastArg.explicitOption] = value;
     }
 
     return out;
--- src/win32/index.js
+++ src/win32/index.js
@@ -1,7 +1,20 @@
 const cp = require('child_process');
 
 module.exports = async args => {
-    args.ports.forEach(killByPort);
+    args.ports.forEach(port => killByPort(port)
+        .then(() => console.log(`Process listening on port ${port} has been terminated.`))
+        .catch(() => console.error(`Could not find a process listening on port ${port}!`))
+    );
+
+    args.processIds.forEach(pid => killPID(pid)
+        .then(() => console.log(`Process ${pid} has been terminated.`))
+        .catch(() => console.error(`Could not find a running process with PID ${pid}!`))
+    );
+
+    args.names.forEach(name => killPIDByName(name).catch()
+        .then(() => console.log(`Process ${name} has been terminated.`))
+        .catch(() => console.error(`Could not find process ${name}!`))
+    );
 }
 
 const execute = command => new Promise((resolve, reject) => cp.exec(command, (err, stdout) => {
@@ -11,20 +24,36 @@ const execute = command => new Promise((resolve, reject) => cp.exec(command, (er
     resolve(stdout);
 }));
 
-const getPID = port => execute(`netstat -ano | findStr "${port}"`).then(out => +out.split('LISTENING')[1].split('\n')[0].trim());
+const getPIDByPort = port => execute(`netstat -ano | findStr "${port}"`).then(out => +out.split('LISTENING')[1].split('\n')[0].trim());
 
 const killPID = pid => execute(`taskkill /F /PID ${pid}`);
 
-const killByPort = async port => {
-    const pid = await getPID(port).catch(() => {
-        console.error(`Could not find a process listening on port ${port}`);
-        return null;
-    });
+const killPIDByName = async name => {
 
-    if (!pid) {
-        return;
+    let ranOnce = false;
+    while (true) {
+        const output = await execute(`tasklist /v /fo csv | findstr /i "${name}"`).catch(() => '');
+        const lines = output.split('\n');
+
+        if (output === '') {
+            if (!ranOnce) {
+                throw "Could not execute command";
+            }
+
+            return;
+        }
+
+        const [_, processName, __, pid] = lines[0].split('"');
+        if (processName !== name) {
+            continue;
+        }
+
+        await killPID(pid);
+        ranOnce = true;
     }
+}
 
+const killByPort = async port => {
+    const pid = await getPIDByPort(port);
     await killPID(pid);
-    console.log(`PID ${pid} listening on port ${port} is now terminated`);
 }
